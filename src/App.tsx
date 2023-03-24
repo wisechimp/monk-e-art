1165195378faab049a65e5b85129acd408e87632
shicks1080 <stuart.hicks@gmail.com>
2023-03-24 14:25:35 +0100

    Squashed commit of the following:
    commit 791652e98c317c7b9a2740853e3e69b3794aad75
    Author: shicks1080 <stuart.hicks@gmail.com>
    Date:   Fri Mar 24 14:24:59 2023 +0100
    
        fix: Adding a minimum character length
    
        This prevents the request to Dall-E breaking.
        Also added the artist himself!
        Moved some text values into a constants file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
-import React, { useEffect, useState } from "react"
+import React, { ChangeEvent, useEffect, useState } from "react"
 
+import constantValues from "./constants/constantValues"
 import generateImage from "./helpers/generateImage"
+import Avatar from "./images/salvator-monke-tm.png"
 import "./App.css"
 
 const App = () => {
   const [textInput, setTextInput] = useState("")
   const [textEntered, setTextEntered] = useState(false)
+  const [buttonDisabled, setButtonDisabled] = useState(true)
   const [imageUrl, setImageUrl] = useState("")
+
+  const {
+    generateImageButtonText,
+    inputPromptText,
+    minimumPromptLength,
+    minimumPromptLengthHelp,
+  } = constantValues
+
+  const handleTextChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setTextInput(event.currentTarget.value)
+  }
 
   const imageGenerationButtonHandler = (event: React.MouseEvent) => {
     event.preventDefault()
     setTextEntered(true)
   }
+
+  useEffect(() => {
+    textInput.length >= minimumPromptLength
+      ? setButtonDisabled(false)
+      : setButtonDisabled(true)
+  }, [textInput, buttonDisabled, minimumPromptLength])
 
   useEffect(() => {
     if (textEntered) {
@@ -26,17 +46,34 @@ const App = () => {
 
   return (
     <div className='App'>
-      <h1 className='customTitle'>Ask Monk-E to create some artwork</h1>
+      <div className='header'>
+        <h1 className='customTitle'>Ask Monk-E to create some artwork</h1>
+        <img
+          className='avatar'
+          src={Avatar}
+          alt='The AI artist generated in AI!'
+        />
+      </div>
       <div className='userInputContainer'>
         <input
           className='userInput'
-          placeholder='Enter text to prompt the Monkey'
-          onChange={(event) => setTextInput(event.target.value)}
+          placeholder={inputPromptText}
+          onChange={handleTextChange}
         />
-        <button className='userButton' onClick={imageGenerationButtonHandler}>
-          Generate Image
+        <p className='userHelpText'>{minimumPromptLengthHelp}</p>
+        <button
+          className='userButton'
+          disabled={buttonDisabled}
+          onClick={imageGenerationButtonHandler}
+        >
+          {generateImageButtonText}
         </button>
       </div>
+      <p className='userInfoText'>
+        It may come as no surprise to learn that Monk-E is an AI artist
+        (Aitist?) built on <a href='https://openai.com/'>OpenAI's</a>{" "}
+        <a href='https://openai.com/product/dall-e-2'>DALL-E</a>.
+      </p>
       {imageUrl.length > 0 ? (
         <img
           className='monkeyImage'
