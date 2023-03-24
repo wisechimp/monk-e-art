791652e98c317c7b9a2740853e3e69b3794aad75
shicks1080 <stuart.hicks@gmail.com>
2023-03-24 14:24:59 +0100

    fix: Adding a minimum character length
    This prevents the request to Dall-E breaking.
    Also added the artist himself!
    Moved some text values into a constants file.

---
+++ src/constants/constantValues.ts
@@ -0,0 +1,8 @@
+const numberValues = {
+  generateImageButtonText: "Generate Image",
+  inputPromptText: "Enter text to prompt the Monk-E*",
+  minimumPromptLength: 5,
+  minimumPromptLengthHelp: "*Min. number of characters is 5",
+}
+
+export default numberValues
